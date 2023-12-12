a98fc9f5b83683fc98c6200147d7eb64455e607b
John Doe <johndoe@email.com>
2023-12-12 15:38:30 -0500

    main

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyCard',
+    version='0.1',
+    author='Patrick Farrell',
+    description='Python Card Game Devolopment',
+    packages=find_packages(),
+    install_requires=[
+        # List any dependencies your module may have
+    ],
+)
