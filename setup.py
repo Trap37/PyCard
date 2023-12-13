9b876218f505a767136bd984a5feb0b36e96260f
John Doe <johndoe@email.com>
2023-12-13 01:01:07 -0500

    main

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='PyCard',
     version='0.1.0',
     packages=find_packages(),
+    install_requires=[
+        'random',
+        'itertools',
+    ],
     author='Patrick Farrell',
     description='A card game library',
     url='https://github.com/Trap37/PyCard',
