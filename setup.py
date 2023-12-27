5ca8d0331b6ae69cd2ef3103f2f1309d12298e3c
John Doe <johndoe@email.com>
2023-12-27 11:33:31 -0500

    main

--- setup.py
+++ setup.py
@@ -3,14 +3,19 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as f:
     LONG_DESCRIPTION = f.read()
 
+PROJECT_NAME = "pycard"
+VERSION_NUMBER = "0.0.1"
+DESCRIPTION = "A free library for all things cards"
+
 setup(
-    name="Pycard",
-    version="0.0.1",
-    description="A free library for all things cards",
-    packages=find_packages(),
+    name=PROJECT_NAME,
+    version=VERSION_NUMBER,
+    description=DESCRIPTION,
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/Trap37/Pycard",
+    url="https://github.com/Trap37/pycard",
     author="Trap37",
     license="MIT",
     classifiers=[
@@ -18,7 +23,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    install_requires=[""],
+    install_requires=[],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0"],
     },
